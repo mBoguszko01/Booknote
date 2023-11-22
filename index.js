1cf81dd0096ecb3f886aa1235465c28406affee0
MICHAL BOGUSZKO <mic.boguszko@gmail.com>
2023-11-22 19:16:59 +0100

    Visual improvemnts

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import bodyParser from "body-parser";
 import axios from "axios";
+import pg from "pg";
 
 const app = express();
 const port = 3000;
@@ -31,6 +32,30 @@ app.get("/profile.ejs", (req,res)=>{
 })
 app.get("/settings.ejs", (req,res)=>{
     res.render("settings.ejs");
+})
+
+app.post("/login", (req,res)=>{
+    console.log(req.body);
+    res.render("user_home.ejs");
+})
+app.post("/register", (req,res)=>{
+    console.log(req.body);
+    res.render("sign_in.ejs");
+})
+app.post("/creatNewPost", (req,res)=>{
+    console.log(req.body);
+    res.render("user_home.ejs");
+})
+app.post("/edit_post", (req,res)=>{
+    console.log(req.body);
+    res.render("edit_post.ejs", 
+    {
+        title: req.body.title,
+        author: req.body.author,
+        finishedReading: req.body.finishedReading,
+        rating: req.body.rating,
+        notes: req.body.notes
+    });
 })
 app.listen(port, ()=>{
     console.log(`Server running on port ${port}`);
